117cb8a6a1a3ad43e103be77f10eef7d05846e88
Vishnu Narayan Tripathi <2023nitsgr146@nitsri.ac.in>
2026-01-16 19:18:54 +0530

    Update Day1.sql

--- Day1/Day1.sql
+++ Day1/Day1.sql
@@ -1,27 +1,28 @@
 CREATE DATABASE intern_training_db;
 USE intern_training_db;
-show tables;
-CREATE TABLE students (
-    id INT AUTO_INCREMENT PRIMARY KEY,   
-    name VARCHAR(100) NOT NULL,           
-    email VARCHAR(150) NOT NULL UNIQUE,  
-    age INT NOT NULL,                     
-    dob DATE NOT NULL                    
+CREATE TABLE students(
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(100),
+email VARCHAR(100),
+age INT
 );
-INSERT INTO students (name, email, age, dob) VALUES
-('Amit Sharma', 'amit@gmail.com', 21, '2003-02-15'),
-('Neha Verma', 'neha@gmail.com', 22, '2002-07-10'),
-('Rahul Singh', 'rahul@gmail.com', 20, '2004-01-05');
-select* from students;
-INSERT INTO students (name, email, age, dob)
-VALUES ('Rohan Gupta', 'amit@gmail.com', 23, '2001-06-01');
-INSERT INTO students (name, email, age)
-VALUES ('Pooja Mehta', 'pooja@gmail.com', 22);
-ALTER TABLE students
-ADD phone_number VARCHAR(15);
-ALTER TABLE students
-DROP COLUMN age;
-DESC students;
+INSERT INTO students (name, email, age) VALUES
+('Amit Sharma', 'amit@gmail.com', 21),
+('Neha Verma', 'neha@gmail.com', 22),
+('Rahul Singh', 'rahul@gmail.com', 20),
+('Pooja Mehta', 'pooja@gmail.com', 23),
+('Rohan Gupta', 'rohan@gmail.com', 21);
+
+SELECT * FROM students;
+SELECT  name FROM students;
+SELECT name from students where age>21;
+SELECT email from students;
+DROP TABLE intern_training_db.students;
+
+
+
+
+
 
 
 
